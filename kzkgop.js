d805051f7c727001c58e98d1c28e13de364856ce
szmeku <szmeku@gmail.com>
2018-02-22 17:21:06 +0100

    Update kzkgop.js

--- kzkgop.js
+++ kzkgop.js
@@ -8,28 +8,30 @@ var querySelectorAll = _.curryN(2, (selector, el) => Array.from(el.querySelector
 
     // retrieveSubSchedules = (el) => Array.from(el.querySelectorAll('.panel')).map(retrieveSubSchedule),
     retrieveSchedules = _.compose(
-        _.objOf('schedules'),
         _.map(_.compose(
             _.mergeAll,
             _.juxt([
                 _.compose(_.objOf('lineNumber'), text, querySelector('.btn-primary')),
                 _.compose(
-                    _.objOf('arrivalTimes'),
+                    _.objOf('subSchedules'),
                     _.map(_.compose(
                         _.mergeAll,
                         _.juxt([
                             _.compose(
-                                _.objOf('dayType'),
+                                _.objOf('name'),
                                 text,
                                 querySelector('.panel-heading .panel-title')
                             ),
                             _.compose(
-                                _.objOf('times'),
+                                _.objOf('arrivalTimes'),
                                 _.map(_.compose(
                                     _.mergeAll,
                                     _.juxt([
                                         _.compose(_.objOf('hour'), text, _.nth(0), _.prop('childNodes')),
-                                        _.compose(_.objOf('min'), text, _.nth(1), _.prop('childNodes'))
+                                        _.compose(
+                                            _.objOf('mins'),
+                                            _.map(_.compose(text, _.nth(0), _.prop('childNodes'), _.nth(1), _.prop('childNodes'))),
+                                            querySelectorAll('sup'))
                                     ])
                                 )),
                                 querySelectorAll('.panel-body .arrival-time')
@@ -41,7 +43,51 @@ var querySelectorAll = _.curryN(2, (selector, el) => Array.from(el.querySelector
             ])
         )),
         querySelectorAll('.container-wide .container'),
+    ),
+
+    schedulesByHours = _.pipe(
+        retrieveSchedules,
+        _.map(_.compose(
+            _.apply(_.map),
+            _.juxt([
+                _.compose(_.assoc('lineNumber'), _.prop('lineNumber')),
+                _.compose(
+                    _.prop('arrivalTimes'),
+                    _.find(_.anyPass([
+                        _.propEq('name', 'Robocze- Kursuje w dni robocze przez cały rok'),
+                        _.propEq('name', 'Codziennie- Kursuje codziennie przez cały rok')
+                    ])),
+                    _.prop('subSchedules')
+                )
+            ])
+        )),
+        _.flatten,
+        _.map(_.compose(
+            _.apply(_.map),
+            _.juxt([
+                _.compose(_.merge, _.pick(['lineNumber', 'hour'])),
+                _.compose(_.map(_.objOf('min')), _.prop('mins'))
+            ])
+        )),
+        _.flatten,
+        _.sortBy(_.pipe(
+            _.props(['hour', 'min']),
+            _.join('.'),
+            Number
+        )),
+        _.groupBy(_.prop('hour')),
+        _.mapObjIndexed(_.pipe(
+                _.pipe(
+                    _.map(_.pipe(
+                        _.props(['min', 'lineNumber']),
+                        _.join(' - ')
+                    )),
+                    _.join(',')
+                ),
+        )),
+        _.values,
+        _.join('\n'),
+        console.log
     );
 
-
-JSON.stringify(retrieveSchedules(document));
+schedulesByHours(document);
